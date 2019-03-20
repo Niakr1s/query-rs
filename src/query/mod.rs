8ce79eb6e8a513178d939ab15ef27772f4631a7d
Niakr1s <pavel2188@gmail.com>
2019-03-20 22:07:32 +0300

    added Query struct that is superior to all *Query

--- src/query/mod.rs
+++ src/query/mod.rs
@@ -1,6 +1,24 @@
-use crate::query_result::QueryResult;
-use crate::text_query::TextQuery;
+use crate::prelude::*;
 
 pub mod no_query;
 pub mod traits;
 pub mod word_query;
+
+pub struct Query<Q>
+where
+    Q: QueryCommon,
+{
+    query: Q,
+}
+
+impl<Q> QueryCommon for Query<Q>
+where
+    Q: QueryCommon,
+{
+    fn eval(&self, tq: &TextQuery) -> QueryResult {
+        self.query.eval(&tq)
+    }
+    fn rep(&self) -> String {
+        self.query.rep()
+    }
+}
